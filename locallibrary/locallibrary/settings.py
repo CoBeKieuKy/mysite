3f9b715faf1ffc36eb747aa81dd19637eaedc532
hoang cong thanh <hoangcongthanh1237d@gmail.com>
2020-08-14 09:27:10 +0700

    fix

--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -119,7 +119,6 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 # Redirect to home URL after login (Default redirects to /accounts/profile/)
