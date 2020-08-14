84a4b0b8c00dc921af9f1bda99ec536c893a122f
hoang cong thanh <hoangcongthanh1237d@gmail.com>
2020-08-14 09:59:40 +0700

    fix

--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -119,6 +119,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Redirect to home URL after login (Default redirects to /accounts/profile/)
